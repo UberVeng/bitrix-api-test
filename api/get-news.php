a878fccaa1972314433dfa2b86def1840fd5d1aa
Andrew Kopel <ankopel@yandex.ru>
2023-10-23 13:12:12 +0700

    Исправления

--- api/get-news.php
+++ api/get-news.php
@@ -5,19 +5,13 @@ require($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.ph
 
 header('Content-Type: application/json; charset=utf-8');
 
-$pageSize = 10;
-$pageNumber = 1;
 $pageSizeLimit = 50;
 
-if (isset($_GET["page_size"]) && is_int((int)$_GET["page_size"]) && $_GET["page_size"] > 0 && $_GET["page_size"] < $pageSizeLimit) {
-	$pageSize = $_GET["page_size"];
-} else {
-	$pageSize = $pageSizeLimit;
-}
+$pageSize = (int)$_GET["page_size"];
+$pageSize = $pageSize > 0  && $pageSize <= $pageSizeLimit ? $pageSize : 10;
 
-if (isset($_GET["page_number"]) && is_int((int)$_GET["page_number"]) && $_GET["page_number"] > 0) {
-	$pageNumber = $_GET["page_number"];
-}
+$pageNumber = (int)$_GET["page_number"];
+$pageNumber = $pageNumber > 0 ? $pageNumber : 1;
 
 $arSelect = Array("NAME", "DATE_CREATE", "DETAIL_PICTURE", "DETAIL_TEXT");
 $arFilter = Array("IBLOCK_ID"=>2, "ACTIVE"=>"Y");
@@ -26,15 +20,9 @@ $out = [];
 
 while($ar = $res->fetch())
 {
- foreach ($ar as $key => $value) {
-	if ($key == "DETAIL_PICTURE") {
-		$ar[$key] = CFile::GetPath($value);
-	}	
-	if ($key == "DETAIL_TEXT_TYPE") {
-		unset($ar[$key]);
-	}
- }
- $out[] = ($ar);
+	$ar["DETAIL_PICTURE"] = CFile::GetPath($ar["DETAIL_PICTURE"]);
+	unset($ar["DETAIL_TEXT_TYPE"]);
+	$out[] = $ar;
 }
 
 echo json_encode($out);
